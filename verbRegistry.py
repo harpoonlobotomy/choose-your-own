74bedaa181e9862feee34432829ac3c79629db05
Gabriel Lee <harpoonlobotomy@gmail.com>
2026-01-01 18:10:27 +0000

    interim verbRegistry update
    Going to try adding dataclasses to make it easier to tokenise the string parts.

--- verbRegistry.py
+++ verbRegistry.py
@@ -12,11 +12,13 @@ class VerbInstance:
         self.id = str(uuid.uuid4())  # unique per instance
         self.name = verb_key
         self.alt_words=attr["alt_words"]
-        self.null_words = attr["null_words"]
-        self.expected_parts = attr.get("expected_parts")  # switching over to make this the 'discovered at' attr.
+        self.null_words = attr["allowed_null"]
+        #self.expected_parts = attr.get("expected_parts")  # switching over to make this the 'discovered at' attr.
         self.format = attr.get("format")
+        self.distinction = attr.get("distinction") if attr.get("distinction") else None
         self.colour = None
 
+#"set": {"alt_words": [""], "allowed_null": None, "format": [verb_noun_dir, verb_noun_sem_noun], "distinction": {"second_noun":"fire", "new_verb":"burn", "else_verb":"put"}}
 
 class VerbRegistry:
     """
@@ -28,13 +30,12 @@ class VerbRegistry:
         #print(f"Init in lootregistry is running now.") ## it only seems to be running once.
         #sleep(1)
 
-        self.verbs = {}      # id -> ItemInstance
-        self.by_name = {}        # definition_key -> set of instance IDs
-        self.by_category = {}        # category (loot value) -> set of instance IDs
+        self.verbs = {}      # id -> VerbInstance
+        self.by_name = {}        # verb_key -> verb
+        self.by_format = {}        # format -> set of verbs
         self.by_alt_words = {}
-        self.requires_noun = list()
-        self.not_requires_noun = list()
         self.list_null_words = set()
+        self.all_verbs = set() ## just a list of all verbs inc alt names
 
     def create_verb_instance(self, verb_key:str, attr:dict)->VerbInstance:
         #logging_fn()
@@ -44,28 +45,24 @@ class VerbRegistry:
         self.by_name.setdefault(verb_key, list()).append(inst)
         for alt_words in attr["alt_words"]:
             if alt_words:
-                if self.by_alt_words.get(alt_words):
+                if self.by_alt_words.get(alt_words): ## does not work if alt words aren't exclusive.
                     print(f"Alt words {alt_words} already exists.")
                 self.by_alt_words[alt_words] = inst
-        if attr["requires_noun"]:
-            self.requires_noun.append(inst)
-        else:
-            self.not_requires_noun.append(inst)
 
         return inst
 
-    def get_key_parts(self, input_str, location = None, inventory = None):
+    def get_key_parts(self, input_str, inventory = None):
         # eg "go to graveyard"
         # "[verb] [location]"
         # Possibly, moving into a more point-and-click direction, if you could click the verb and/or the obj, then it would be a list of parts pre-made, not a string that needed pulling apart. not doing that here now, but would be an interesting direction. Like the old monkey island style.
 
         cleaned_parts = {}
         parts = input_str.split()
-        print(f"Parts: {parts}")
-        print(f"self.null_words: {self.list_null_words}")
+        #print(f"Parts: {parts}")
+        #print(f"self.null_words: {self.list_null_words}")
         for i, part in enumerate(parts):
             if part in self.list_null_words:
-                print(f"part `{part}` is in list_null_words")
+                #print(f"part `{part}` is in list_null_words")
                 cleaned_parts[i] = {"null":part}
             elif self.by_name.get(part):
                 cleaned_parts[i] = {"verb":self.by_name[part]}
@@ -75,8 +72,12 @@ class VerbRegistry:
                 from item_management_2 import registry
                 noun_options = registry.instances_by_name(part)
                 if noun_options:
+                    if inventory:
+                        if part in inventory:
+                            cleaned_parts[i] = list([x for x in inventory if x == part])[0] # not sure if this is right.
+                    if not cleaned_parts.get(i):
                     ## use location/inventory here later instead of defaulting to first option. (So the string-sender can say 'I'm at {place}', and we refine using that datapoint, as the items_at_here does for iteminstances.)
-                    cleaned_parts[i] = {"noun": noun_options[0]}
+                        cleaned_parts[i] = {"noun": noun_options[0]}
                 else:
                     from env_data import dataset
                     loc_options = list(dataset.keys())
@@ -91,8 +92,6 @@ class VerbRegistry:
         print(f"Cleaned parts: {cleaned_parts}")
 
     def match_format(self, input_str:str):
-        # eg "go to graveyard"
-        # eg "[verb] [location]"
 
         if not isinstance(input_str, str):
             print(f"match_format requires a string input, not {type(input_str)}")
@@ -128,7 +127,9 @@ inventory = []
 
 def initialise_registry():
     from verb_definitions import get_verb_defs
-    for item_name, attr in get_verb_defs().items():
+    verb_defs_dict, verb_set = get_verb_defs()
+    verbs.all_verbs = verb_set
+    for item_name, attr in verb_defs_dict.items():
         verbs.create_verb_instance(item_name, attr)
         if attr.get("null_words"):
             for word in attr.get("null_words"):
@@ -141,8 +142,6 @@ def initialise_registry():
 initialise_registry()
 print("verbs: ")
 print(verbs.by_name)
-print("requires noun:")
-print(verbs.requires_noun)
 print("verbs by alt_words:")
 print(verbs.by_alt_words)
 
@@ -151,3 +150,4 @@ print("go to the museum")
 verbs.match_format("go to the museum")
 
 verbs.get_key_parts("go to the graveyard")
+
--- verb_definitions.py
+++ verb_definitions.py
@@ -4,23 +4,115 @@
 from item_management_2 import ItemInstance
 #from verbRegistry import VerbInstance
 
+#null = "[null]"
+#verb = "[verb]"
+#location = "[location]"
+#noun = "[noun]"
+
+null = "null"
+verb = "verb"
+location = "location"
+noun = "noun"
+sem = "sem" # semantic operator (with, to, from etc)
+direction = "direction"
+
+# o[type] == 'optional {type}'
+
+#formats = {
+#    "verb_only": f"{verb}",
+#    "verb_noun": f"{verb} o{null} {noun}",
+#    "verb_loc": f"{verb} o{null} {location}",
+#    "verb_noun_noun": f"{verb} o{null} {noun} {null} o{null} {noun}"
+#}
+cardinals = ["north", "east", "south", "west"]
+directions = ["down", "up", "left", "right", "away", "toward", "towards", "closer", "further", "to", "against", "across"]
+
+directions = directions + cardinals
+
+
+formats = {
+    "verb_only": (verb),
+    "verb_noun": (verb, noun),
+    "verb_loc": (verb, location),
+    "verb_noun_noun": (verb, noun, noun), # can't think of any examples.
+    "verb_noun_dir": (verb, noun, direction), # throw ball up
+    "verb_noun_dir_noun": (verb, noun, direction, noun), # push chest towards door
+    "verb_noun_sem_noun": (verb, noun, sem, noun)
+}
+
+### 'sem' == semantic operators, eg 'with' in 'combine x with y'.
+verb_only = formats["verb_only"]
+verb_noun = formats["verb_noun"]
+verb_loc = formats["verb_loc"]
+verb_noun_noun = formats["verb_noun_noun"]
+verb_noun_dir = formats["verb_noun_dir"]
+verb_noun_dir_noun = formats["verb_noun_dir_noun"]
+verb_noun_sem_noun = formats["verb_noun_sem_noun"]
+
+## Note: Need to figure out how I'm getting noun-objects in here. Like, 'magnifying glass' is 1 noun, but two words. Need to figure that out.
+
+# {'and', 'with', 'to', 'the', 'at', 'plus', 'a'}, from, out, of,  "on", "against" ### Note: There may be more than one viable semantic when only one is required. That's fine.
+# So if it expects 'verb noun sem noun'
+# and the input is
+# take flowers from in the vase
+# while technically that's three viable semantics (and two that would be viable for 'vase'),
+# it meets the requirement of 'one' - exceeding is not exclusion for semantics.
+
+
+# "from" (eg 'remove flowers from the vase')
+# "inside" eg ("look inside the book" == "look at book")
+# if no 'allowed_null', all null = allowed.
+
 verb_defs_dict = {
-    "go to": {"alt_words":["go", "approach"], "null_words": ["to"], "expected_parts":1, "format": f"[verb] [location]", "requires_noun": True},
-    "leave": {"alt_words": ["depart", ""], "null_words": ["the"], "expected_parts":1, "format": f"[verb] [location]", "requires_noun": False},
-    "combine": {"alt_words": ["mix", "add"], "null_words": ["and", "with", "plus", "the", "a"], "expected_parts": 2, "format": f"[verb] [null] {type(ItemInstance)} [null] {type(ItemInstance)}", "requires_noun": True},
-    "drop": {"alt_words": ["", ""], "null_words": ["the", "a"], "expected_parts": 1, "format": f"[verb] {type(ItemInstance)}", "requires_noun": True},
-    "eat": {"alt_words": ["consume", ""], "null_words": ["the", "a"], "expected_parts": 1, "format": f"[verb] {type(ItemInstance)}", "requires_noun": True},
-    "look": {"alt_words": ["watch", "observe"], "null_words": ["at"], "expected_parts": 1, "format": f"[verb] {type(ItemInstance)}", "requires_noun": False}
+    "go to": {"alt_words":["go", "approach"], "allowed_null": None, "formats": [verb_loc]},
+    "leave": {"alt_words": ["depart", ""], "allowed_null": None, "formats": [verb_only, verb_loc]},
+    "combine": {"alt_words": ["mix", "add"], "allowed_null": None, "formats": [verb_noun_sem_noun]},
+    "separate": {"alt_words": ["remove", "get"], "allowed_null": None, "formats": [verb_noun_sem_noun]},
+    "throw": {"alt_words": ["chuck", "lob"], "allowed_null": None, "formats": [verb_noun, verb_noun_dir, verb_noun_sem_noun]}, # throw ball down, throw ball at tree
+    "push": {"alt_words": ["shove", "move", "pull"], "allowed_null": None, "format": [verb_noun, verb_noun_dir, verb_noun_dir_noun]},
+    "drop": {"alt_words": ["discard", ""], "allowed_null": None, "format": [verb_noun]},
+    "open": {"alt_words": ["", ""], "allowed_null": None, "format": [verb_noun]},
+    "read": {"alt_words": ["examine", ""], "allowed_null": None, "format": [verb_noun]}, ## Two nouns have 'examine'. Maybe make 'read' its own specific thing instead of referring 'examine' here. idk.
+    "burn": {"alt_words": ["", ""], "allowed_null": None, "format": [verb_noun, verb_noun_sem_noun], "inventory_check": "fire_source"},
+    "lock": {"alt_words": ["", ""], "allowed_null": None, "format": [verb_noun, verb_noun_sem_noun], "inventory_check": "key"},
+    "unlock": {"alt_words": ["", ""], "allowed_null": None, "format": [verb_noun_sem_noun], "inventory_check": "key"},
+    "open": {"alt_words": ["pry", ""], "allowed_null": None, "format": [verb_noun, verb_noun_sem_noun]},
+    "break": {"alt_words": ["smash", ""], "allowed_null": None, "format": [verb_noun, verb_noun_sem_noun]},
+    "take": {"alt_words": ["pick up", "get", "pick"], "allowed_null": None, "format": [verb_noun, verb_noun_sem_noun]}, # take ball, take ball from bag
+    "put": {"alt_words": ["place", "leave"], "allowed_null": ["on", "in", "inside"], "format": [verb_noun_dir, verb_noun_sem_noun]}, # put paper down, put paper on table ## using 'leave' here might be tricky. But I want to allow for 'leave church' and 'leave pamphlet on table' both.
+    "eat": {"alt_words": ["consume", "drink"], "allowed_null": None, "format": [verb_noun]},
+    "look": {"alt_words": ["watch", "observe", "investigate", "examine"], "allowed_null": ["at", "to", "with"],  "format": [verb_only, verb_noun, verb_noun_sem_noun]}, # look, look at book, look at book with magnifying glass
+    "set": {"alt_words": [""], "allowed_null": None, "format": [verb_noun_dir, verb_noun_sem_noun], "distinction": {"second_noun":"fire", "new_verb":"burn", "else_verb":"put"}} ## not implemented, just an idea. 'if fire is the second noun, the correct verb to use is 'burn', else the verb is 'put'. So 'set' is not its own thing, just a membrane/signpost.
     }
+
+### how to deal with two-part 'word phrases' (eg 'pick up')? Initial thought, just use the first word and check the second (so 'pick up the book', 'pick' is the verb, and we check it's 'pick up' once 'pick' is identified to make sure it's right. Though that might not even be necessary, 'pick' isn't exactly its own separate verb)
+## also, how to deal with something like 'set paper on fire'
+# Need a clean way to deal with multi-component things like this. Say, 'set', could be 'set paper on table', or 'set paper on fire'. So maybe
+# 'if set, if input[noun2] == "fire", verb = burn. else, verb = place
+
+#verb_defs_dict = {
+#    "go to": {"alt_words":["go", "approach"], "null_words": ["to"], "formats": [verb_only, verb_noun]},
+#    "leave": {"alt_words": ["depart", ""], "null_words": ["the"], "formats": [verb_noun]},
+#    "combine": {"alt_words": ["mix", "add"], "null_words": ["and", "with", "plus", "the", "a"], "expected_parts": 2, "format": f"[verb] [null] {type#(ItemInstance)} [null] {type(ItemInstance)}", "requires_noun": True},
+#    "drop": {"alt_words": ["", ""], "null_words": ["the", "a"], "expected_parts": 1, "format": f"[verb] {type(ItemInstance)}", "requires_noun": True},
+#    "eat": {"alt_words": ["consume", ""], "null_words": ["the", "a"], "expected_parts": 1, "format": f"[verb] {type(ItemInstance)}", "requires_noun": True},
+#    "look": {"alt_words": ["watch", "observe"], "null_words": ["at"], "expected_parts": 1, "format": f"[verb] {type(ItemInstance)}", "requires_noun": False}
+#    }
 
 
 def get_verb_defs(verb_name=None):
     #print("\n" * 10)
+    verb_set=set()
     if verb_name:
         attr=verb_defs_dict.get(verb_name)
         return attr
     else:
-        #for verb, attr in verb_defs_dict.items():
+        for verb, attr in verb_defs_dict.items():
+            verb_set.append(verb)
+            for alt_name in attr["alt_words"]:
+                if alt_name:
+                    verb_set.add(alt_name)
         #    #print(f"verb: {verb}, attr: {attr}")
         #    verb_data = verb_defs_dict[verb]
-        return verb_defs_dict
+
+        return verb_defs_dict, verb_set ## Just a strait up set of all the verbs, nothing else.
